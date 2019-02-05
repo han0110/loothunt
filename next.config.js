5a867330578e88425a9a71f05cb79f4724590429
han0110 <qwerzxcv0110@gmail.com>
2019-02-05 23:34:48 +0800

    :alien: Remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 
-const reponame = 'loothunt'
-
 module.exports = withSass(
   withImages({
-    assetPrefix: process.env.NODE_ENV === 'production' ? `/${reponame}` : '',
     cssModules: true,
     cssLoaderOptions: {
       localIdentName: '[name]___[local]___[hash:base64:5]',
